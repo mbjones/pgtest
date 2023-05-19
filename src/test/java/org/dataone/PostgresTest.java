e41cc7e62bf4a40ceda3e0b35fa86d855b7670ea
Matt Jones <gitcode@magisa.org>
2023-05-19 15:58:10 -0800

    Add basic SQL test.

--- src/test/java/org/dataone/PostgresTest.java
+++ src/test/java/org/dataone/PostgresTest.java
@@ -2,8 +2,14 @@ package org.dataone;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 import org.junit.After;
@@ -11,6 +17,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 // import org.testcontainers.db.AbstractContainerDatabaseTest;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * Basic Postgres test using TestContainers.
@@ -66,16 +75,54 @@ public class PostgresTest {
         flyway.migrate();
     }
 
-    // public void select() {
-    //     ResultSet resultSet = performQuery(pg, "SELECT 1");
-    //     int resultSetInt = resultSet.getInt(1);
-    //     assertTrue(resultSetInt==1);
-    // }
+    @Test
+    public void select() {
+        try {
+            ResultSet resultSet = runQuery(pg, "SELECT 1");
+            int resultSetInt = resultSet.getInt(1);
+            assertTrue(resultSetInt == 1);
+            resultSet.close();
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+
+        // try {
+        //     String sql = "SELECT object amount, name FROM products WHERE amount = 126000";
+        //     ResultSet resultSet = runQuery(pg, sql);
+        //     String object = resultSet.getString(1);
+        //     int amount = resultSet.getInt(2);
+        //     String name = resultSet.getString(3);
+        //     assertTrue(object.equals("product"));
+        //     assertTrue(amount == 126000);
+        //     assertTrue(name.equals("Additional Data Storages"));
+        //     resultSet.close();
+        // } catch (SQLException e) {
+        //     fail(e.getMessage());
+        // }
+    }
 
     @After
     public void tearDownAll() {
 
         // Close the database
         pg.close();
+    }
+
+    private ResultSet runQuery(PostgreSQLContainer<?> container, String sql) throws SQLException {
+        DataSource ds = dsFromContainer(container);
+        Statement statement = ds.getConnection().createStatement();
+        statement.execute(sql);
+        ResultSet results = statement.getResultSet();
+        results.next();
+        return results;
+    }
+
+    private DataSource dsFromContainer(PostgreSQLContainer<?> container) {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(container.getDriverClassName());
+        config.setJdbcUrl(container.getJdbcUrl());
+        config.setUsername(container.getUsername());
+        config.setPassword(container.getPassword());
+        return new HikariDataSource(config);
     }
 }
